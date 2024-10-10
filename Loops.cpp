6d05c0feaf7d38d14e1b34468541233aaa74fbfd
Hassan <hassanjaan731@gmail.com>
2024-10-10 14:11:51 +0500

    Adding All loops Questions

---
+++ Loops.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	//1
+	/*for (int i = 1; i < 101; i++)
+	{
+		cout << i << " \t";
+	}*/
+
+	//2
+	/*for (int i = 100; i > 0; i--)
+	{
+		cout << i << " \t";
+	}*/
+
+	//3
+	/*for (int i = 20; i >= 2; i -= 2)
+	{
+		cout << i << "\t";
+	}*/
+
+	//4
+	//for (int i = 2; i <= 20; i+=3)
+	//{
+	//	cout << i << "\t";
+	//}
+
+	//5
+
+	for (int i = 99; i >= 0; i-=11)
+	{
+		cout << i << "\t";
+	}
+
+
+}
